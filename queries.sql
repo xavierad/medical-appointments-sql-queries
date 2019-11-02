218cbc2236f122ce24c93b6e8dc9e7bc01c64da7
xavierad <xdias_28@hotmail.com>
2019-11-02 19:36:05 +0000

    Update queries.sql

--- queries.sql
+++ queries.sql
@@ -1,7 +1,11 @@
 
 -- 1
-select client.VAT, client.name, phone_number_client.phone_number
-from  consultation natural join doctor on VAT_doctor
-where doctor.VAT = employee.VAT and employee.name = 'Jane Sweettooth';
+select C.VAT, C.name, PN.phone_number
+from  client as C, phone_number_client as PN, appointment as A, consultation as C, doctor as D, employee as E
+where C.VAT = A.VAT_client and A.VAT_doctor = C.VAT_doctor and C.VAT_doctor = D.VAT and D.VAT = E.VAT 
+order by client.name;
 
 --2
+select employee.name, trainee_doctor.VAT, employee.name --?  
+from supervision_report as SP, 
+where supervision_report.evaluation < 3 or supervision_report.description = 'insufficient';
