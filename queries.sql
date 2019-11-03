4581e7e6a2128767cc1f1d45fe2fac8fd21b4a30
xavierad <xdias_28@hotmail.com>
2019-11-03 21:31:31 +0000

    Update queries.sql

--- queries.sql
+++ queries.sql
@@ -6,6 +6,22 @@ where C.VAT = A.VAT_client and A.VAT_doctor = C.VAT_doctor and C.VAT_doctor = D.
 order by client.name;
 
 --2
-select employee.name, trainee_doctor.VAT, employee.name --?  
-from supervision_report as SP, 
-where supervision_report.evaluation < 3 or supervision_report.description = 'insufficient';
+select employee.name, trainee_doctor.VAT, employee.name, supervision_report.evaluation, supervision_report.description --employee.name: nome do trainee e nome do doctor
+from supervision_report as SP, trainee_doctor as TD, permanent_doctor as PD, doctor as D, employee as E
+where (supervision_report.evaluation < 3 or supervision_report.description = '%insufficient') and  SP.VAT = TD.VAT and TD.VAT = D.VAT and D.VAT = E.VAT and TD.supervisor = PD.VAT and PD.VAT = D.VAT and D.VAT = E.VAT
+order by supervision_report.evaluation desc;
+
+--3
+select client.name, client.city, client.VAT_client
+from client, appointment, consultation
+where appointment.VAT_doctor = consultation.VAT_doctor and consultation.date_timestamp 
+
+
+
+
+
+
+
+
+
+
