914a3b8529ff5be07c1e8b0cd2943327cfbcde3c
pedrogcmartin <48490908+pedrogcmartin@users.noreply.github.com>
2019-11-05 23:43:13 +0000

    Update queries.sql

--- queries.sql
+++ queries.sql
@@ -3,8 +3,8 @@
 select distinct client.VAT, client._name, PN.phone
 from  (client natural join phone_number_client as PN), (appointment right outer join consultation on appointment.date_timestamp=consultation.date_timestamp)
 where  client.VAT = appointment.VAT_client and consultation.VAT_doctor = all(select employee.VAT
-                                                  from employee
-                                                  where employee._name = 'Jane Sweettooth')
+from employee
+where employee._name = 'Jane Sweettooth')
 order by client._name;
 
 -- 2
@@ -12,4 +12,11 @@ select E_t._name, SR.VAT, E_p._name, SR.evaluation, SR._description
 from ((supervision_report as SR) natural left outer join trainee_doctor), employee as E_p, employee as E_t
 where trainee_doctor.VAT=E_t.VAT and trainee_doctor.supervisor = E_p.VAT and (SR.evaluation < 3 or SR._description like '%insufficient')
 order by SR.evaluation desc;
+
 --3
+select client._name, client.VAT, client.city, client.zip
+from client, appointment, consultation
+where client.VAT=appointment.VAT_client
+and appointment.VAT_doctor=consultation.VAT_doctor
+and appointment.date_timestamp=consultation.date_timestamp
+and (SOAP_O like '%gingivitis' or SOAP_O like '%periodontitis');
