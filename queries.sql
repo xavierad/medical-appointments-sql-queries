ac988f5124f1f22fdbcf61c1ffc1645600599c17
pedrogcmartin <48490908+pedrogcmartin@users.noreply.github.com>
2019-11-06 02:03:55 +0000

    Update queries.sql

--- queries.sql
+++ queries.sql
@@ -13,10 +13,20 @@ from ((supervision_report as SR) natural left outer join trainee_doctor), employ
 where trainee_doctor.VAT=E_t.VAT and trainee_doctor.supervisor = E_p.VAT and (SR.evaluation < 3 or SR._description like '%insufficient')
 order by SR.evaluation desc;
 
---3
-select client._name, client.VAT, client.city, client.zip
+-- 3
+select client._name, client.city, client.VAT
 from client, appointment, consultation
 where client.VAT=appointment.VAT_client
 and appointment.VAT_doctor=consultation.VAT_doctor
 and appointment.date_timestamp=consultation.date_timestamp
 and (SOAP_O like '%gingivitis' or SOAP_O like '%periodontitis');
+
+-- 4
+select client._name, client.VAT, client.street, client.city, client.zip
+from client, appointment
+left join consultation
+on appointment.date_timestamp=consultation.date_timestamp
+and appointment.VAT_doctor=consultation.VAT_doctor
+where consultation.date_timestamp is null
+and consultation.VAT_doctor is null
+and client.VAT=appointment.VAT_client;
