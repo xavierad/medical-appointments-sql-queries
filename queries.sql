8b6bb812b08b0191a60d9de620b6a601a8899ead
xavierad <xdias_28@hotmail.com>
2019-11-06 15:21:56 +0000

    Feito índices Indetação em queries 1 e 2

--- queries.sql
+++ queries.sql
@@ -1,24 +1,26 @@
 
 -- 1
-select distinct client.VAT, client._name, PN.phone
+select distinct client.VAT, client._name as name, PN.phone
 from  (client natural join phone_number_client as PN),
       (appointment right outer join consultation on appointment.date_timestamp=consultation.date_timestamp)
 where  client.VAT = appointment.VAT_client and consultation.VAT_doctor = all(select employee.VAT
                                                                              from employee
                                                                              where employee._name = 'Jane Sweettooth')
 order by client._name;
+-- index: employee._name
 
 -- 2
-select E_t._name, SR.VAT, E_p._name, SR.evaluation, SR._description
+select E_t._name as trainee_name, SR.VAT as trainee_VAT, E_p._name as permanent_name, SR.evaluation, SR._description as description
 from ((supervision_report as SR) natural left outer join trainee_doctor),
      employee as E_p, employee as E_t
-where trainee_doctor.VAT=E_t.VAT
+where (SR.evaluation < 3 or SR._description like '%insufficient')
       and trainee_doctor.supervisor = E_p.VAT
-      and (SR.evaluation < 3 or SR._description like '%insufficient')
+      and trainee_doctor.VAT=E_t.VAT
 order by SR.evaluation desc;
+-- indexes: SR.evaluation, SR._description
 
 -- 3
-select client._name, client.city, client.VAT
+select client._name as name, client.city, client.VAT
 from client, appointment, consultation
 where client.VAT=appointment.VAT_client
 and appointment.VAT_doctor=consultation.VAT_doctor
@@ -26,7 +28,7 @@ and appointment.date_timestamp=consultation.date_timestamp
 and (SOAP_O like '%gingivitis' or SOAP_O like '%periodontitis');
 
 -- 4
-select client._name, client.VAT, client.street, client.city, client.zip
+select client._name as name, client.VAT, client.street, client.city, client.zip
 from client, appointment
 left join consultation
 on appointment.date_timestamp=consultation.date_timestamp
