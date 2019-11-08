c2f037b5e9a3b8d666726856c1a8c582af89702f
xavierad <xdias_28@hotmail.com>
2019-11-08 14:26:59 +0000

    A corrigir o delete, changes 3

--- Indexes_database.sql
+++ Indexes_database.sql
@@ -1,40 +1,11 @@
-/*
-Para melhorar um desempenho da procura na base de dados para as duas primeiras
-'queries', sugere-se a utilização de índices para a base de dados.*/
 
 /*
 -Query 1:
-Para uma análise do desempenho da procura sem a utilização de índices, resolvemos
-utilizar o comando 'explain' antes da query. Feito isto, obtivémos uma procura
-estimada em todas as linhas de todas as tabelas. Com isto sugerimos indexar apenas
-os atributos (colunas) das tabelas que são procurados através de WHEREs
-e de JOINs.
-Ora como nas 'WHERE clauses' se procura os atributos employee._name, então
-criamos os índices nas colunas das tabelas correspondentes. Quando se procura
-por um valor de um atributo que seja uma chave primária, então não será
-necessário criar um índice, pois são clustered indexes. É o caso das seguintes
-chaves utilizadas: client.VAT, phone_number_client.VAT, phone_number_client.phone,
-appointment.VAT_client, appointment.date_timestamp, consultation.date_timestamp,
-consultation.VAT_doctor. Portanto, a única necessário indexar é employee._name.
-As instruções para a implementação dos índices estão escritas abaixo:
 */
 create index employeeName_idx on employee(_name);
 
 /*
 -Query 2:
-Para uma análise do desempenho da procura sem a utilização de índices, resolvemos
-utilizar o comando 'explain' antes da query. Feito isto, obtivémos uma procura
-estimada em todas as linhas de todas as tabelas. Com isto sugerimos indexar apenas
-os atributos (colunas) das tabelas que são procurados através de WHEREs
-e de JOINs.
-Ora como nas 'WHERE clauses' se procura os atributos employee._name, então
-criamos os índices nas colunas das tabelas correspondentes. Quando se procura
-por um valor de um atributo que seja uma chave primária ou estrangeira, então
-não será necessário criar um índice, pois são clustered indexes. É o caso das
-seguintes chaves: employee.VAT, trainee_doctor.VAT e trainee_doctor.supervisor.
-Portanto, para optimizar, indexamos: supervision_report.evaluation e
-supervision_report._description.
-As instruções para a implementação dos índices estão escritas abaixo:
 */
 create index srEval_idx on supervision_report(evaluation);
 create index srDesc_idx on supervision_report(_description);
--- changes.sql
+++ changes.sql
@@ -12,14 +12,31 @@ set employee.salary =
           from appointment
           where extract(year from appointment.date_timestamp)='2019' and employee.VAT=appointment.VAT_doctor
           group by appointment.VAT_doctor) > 100 then employee.salary*1.05
-    else employee.salary
+    else employee.salary -- necessario, senao preenche com NULLs
   end;
 
 
 -- 3 Del doctor jane sweettoth, removing appoint. & consult.(proced., diagn., prescr.)
-delete from (employee natural join doctor), appointment, consultation,
-            procedure_in_consultation, procedure_charting, procedure_radiology,
-            prescription
-where doctor.VAT=(select employee.VAT
-                  from employee
-                  where employee._name = 'Jane Sweettooth')
+-- a corrigir!!
+delete from _procedure
+where (select count(*)
+          from procedure_in_consultation
+          where procedure_in_consultation.VAT_doctor=(select employee.VAT
+                                                      from employee
+                                                     where employee._name='Jane Sweettooth')
+          group by procedure_in_consultation._name)=1;
+
+delete from diagnostic_code
+where 1=
+  case
+    when (select count(*)
+          from consultation_diagnostic
+          where consultation_diagnostic.VAT_doctor=(select employee.VAT
+                                                      from employee
+                                                      where employee._name='Jane Sweettooth')
+          group by consultation_diagnostic.ID)=1 then 1
+    else 0
+  end;
+
+delete from employee, _procedure
+where employee._name = 'Jane Sweettooth';
